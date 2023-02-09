1090f0d58f5edefbc0c9bfd47007d614d5973979
Valeriya Druchinina <vchumack@glmedia.org>
2023-02-09 03:51:10 +0200

    fixed conflict in contactUs section

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -4,42 +4,36 @@ import { ReactComponent as Tg } from "../../assets/images/contacts/bxl-telegram.
 import style from "../ContactUs/ContactUs.module.scss";
 
 export const ContactUs = () => {
-<<<<<<< HEAD
-  return (
-    <section id="contact" className={style.contactUs}>
-      <div className="container">
-        <div className={style.box}>
-          <div className={style.contactBox}>
-            <div className={style.descriptionBox}>
-              <h2 className={style.title}>LEAVE THE APPLICATION</h2>
-              <p className={style.text}>
-                We will discuss the idea, calculate the benchmark for the cost
-                and development dates of MVP, draw up a work plan
-              </p>
-            </div>
-            <ul className={style.list}>
-              <li className={style.item}>
-                <Email className={style.svg} />
-
-                <a href="/">info@onitech.com</a>
-              </li>
-              <li className={style.item}>
-                <Tg className={style.svg} />
-                <a href="/">Write to telegram</a>
-              </li>
-            </ul>
-          </div>
-          <ContactForm />
-        </div>
-      </div>
-    </section>
-  );
-=======
 	return (
-		<>
-			<div></div>
-			<ContactForm />
-		</>
+		<section id="contact" className={style.contactUs}>
+			<div className="container">
+				<div className={style.box}>
+					<div className={style.contactBox}>
+						<div className={style.descriptionBox}>
+							<h2 className={style.title}>
+								LEAVE THE APPLICATION
+							</h2>
+							<p className={style.text}>
+								We will discuss the idea, calculate the
+								benchmark for the cost and development dates of
+								MVP, draw up a work plan
+							</p>
+						</div>
+						<ul className={style.list}>
+							<li className={style.item}>
+								<Email className={style.svg} />
+
+								<a href="/">info@onitech.com</a>
+							</li>
+							<li className={style.item}>
+								<Tg className={style.svg} />
+								<a href="/">Write to telegram</a>
+							</li>
+						</ul>
+					</div>
+					<ContactForm />
+				</div>
+			</div>
+		</section>
 	);
->>>>>>> 274b95ebe32de9466107be01290ad07c5d897fca
 };
