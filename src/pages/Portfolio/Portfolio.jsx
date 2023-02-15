d800dc53715a3bb19524f24cb2f0658c410ff1da
Valeriya Druchinina <vchumack@glmedia.org>
2023-02-15 23:22:39 +0200

    fixed title styles

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -7,7 +7,7 @@ export const Portfolio = () => {
 		// <section className={style.portfolio} id="portfolio">
 
 		<div className="container">
-			<h2 className={style.portfolio__title}>OUR PORTFOLIO</h2>
+			<h2 className={`${style.portfolio__title} title`}>OUR PORTFOLIO</h2>
 			<PortfolioList />
 		</div>
 		// </section>
