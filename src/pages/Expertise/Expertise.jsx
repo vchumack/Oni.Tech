ee2e60561604c108a67a878267c3ce7e54364f57
Pavlo Vovzhynskyy <pavlovovzhynskyy@MacBook-Air-Pavlo.local>
2023-02-17 19:25:12 +0100

    fix benefits hover

--- src/pages/Expertise/Expertise.jsx
+++ src/pages/Expertise/Expertise.jsx
@@ -9,30 +9,37 @@ export const Expertise = () => {
 	return (
 		// <section className={style.section} current="2" id="expertise">
 		<>
-			<div className="container">
-				<h2 className={`${style.heading} title`}>
-					BENEFITS OF WORKING WITH US
-				</h2>
-				{isBigScreen && (
-					<div className={`${style.back} `}>
-						<p>why</p>
-						<p>us</p>
-					</div>
-				)}
-				<ul className={style.whyList}>
-					{whyUs.map((item) => (
-						<li key={item.key} className={`${style.whyItem}`}>
-							<div className={style.whyMeta}>
-								<div>
-									{<item.icon width="100%" height="100%" />}
+			<section className="section expertise">
+				<div className="container">
+					<h2 className={`${style.heading} title`}>
+						BENEFITS OF WORKING WITH US
+					</h2>
+					{isBigScreen && (
+						<div className={`${style.back} `}>
+							<p>why</p>
+							<p>us</p>
+						</div>
+					)}
+					<ul className={style.whyList}>
+						{whyUs.map((item) => (
+							<li key={item.key} className={`${style.whyItem}`}>
+								<div className={style.whyMeta}>
+									<div>
+										{
+											<item.icon
+												width="100%"
+												height="100%"
+											/>
+										}
+									</div>
+									<span>{item.cardNum}</span>
 								</div>
-								<span>{item.cardNum}</span>
-							</div>
-							<p className={style.whyName}>{item.text}</p>
-						</li>
-					))}
-				</ul>
-			</div>
+								<p className={style.whyName}>{item.text}</p>
+							</li>
+						))}
+					</ul>
+				</div>
+			</section>
 		</>
 		// </section>
 	);
